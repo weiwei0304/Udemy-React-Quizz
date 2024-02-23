f36b55d2a1fd157b30bf8f478544aa2bcdc56148
weiwei0304 <weiwendy0430@gmail.com>
2024-02-23 18:09:47 +0800

    course 192

--- src/App.js
+++ src/App.js
@@ -1,20 +1,50 @@
-import { useEffect } from 'react';
+import { useEffect, useReducer } from 'react';
 import Header from './Header';
 import Main from './Main';
+import Loader from './Loader';
+import Error from './Error';
+
+const initialState = {
+  question: [],
+
+  // 'loading','error','ready','active','finished'
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      };
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      };
+    default:
+      throw new Error('Action unknown');
+  }
+}
+
 export default function App() {
+  const [{ question, status }, dispatch] = useReducer(reducer, initialState);
+
   useEffect(function () {
     fetch('http://localhost:8000/questions')
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error('Error'));
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((err) => dispatch({ type: 'dataFailed' }));
   }, []);
 
   return (
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
       </Main>
     </div>
   );
