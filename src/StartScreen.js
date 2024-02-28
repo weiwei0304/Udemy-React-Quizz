b9d4143f8dc3506a3945ffae14d96490a5b89325
weiwei0304 <106504956+weiwei0304@users.noreply.github.com>
2024-02-28 14:03:19 +0800

    course 192 4:33

---
+++ src/StartScreen.js
@@ -0,0 +1,11 @@
+function StartScreen() {
+  return (
+    <div>
+      <h2>Welcome to the React Quiz!</h2>
+      <p>X question to test your react mastery</p>
+      <button>Let's start</button>
+    </div>
+  );
+}
+
+export default StartScreen;
